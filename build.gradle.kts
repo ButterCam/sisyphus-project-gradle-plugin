cf09bae94fb0dead74ce50af0767b79f326a34d0
higan <higan@live.cn>
2023-10-24 16:12:51 +0800

    :arrow_up: Release 2.1.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "com.bybutter.sisyphus.tools"
-version = "2.0.0"
+version = "2.1.0"
 description = "Plugin for easy configuring Gradle and plugins in Sisyphus Framework"
 
 repositories {
